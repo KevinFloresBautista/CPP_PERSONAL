67c1d34c8a1d3c2a702baa4d51888351ac476625
KevinFloresBautista <kevinflores152017@gmail.com>
2023-05-21 02:44:42 -0300

    nuevo archivo

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    cout << "HOLA" <<endl;
+    return 0;   
+}
